8155aaf5b0caccf8e398a797e038dcad6d2d668b
Koichi Taura <taura.koichi@gmail.com>
2019-06-23 21:27:02 +0900

    add red pattern for test failure of 1 requirement

---
+++ main/fizz_buzz.py
@@ -0,0 +1,3 @@
+class FizzBuzz:
+    def get_fizzbuzz(self, num):
+        return
--- test/fizz_buzz_test.py
+++ test/fizz_buzz_test.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
+from main.fizz_buzz import FizzBuzz
 
 
 class FizzBuzzTest(TestCase):
-    def test_失敗するテスト(self):
-        self.assertTrue(False, "失敗するテスト")
+    def test_1を入力した時に文字列で返す(self):
+        # 準備
+        fizzbuzz = FizzBuzz()
+        #
+        # 実行 & 検証
+        self.assertEqual("1", fizzbuzz.get_fizzbuzz(1))
