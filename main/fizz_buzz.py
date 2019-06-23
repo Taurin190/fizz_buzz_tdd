570b0b37d6498dceb8af71eb071b608c7086763e
Koichi Taura <taura.koichi@gmail.com>
2019-06-23 22:12:57 +0900

    add green pattern for 15 times requirement

--- main/fizz_buzz.py
+++ main/fizz_buzz.py
@@ -1,5 +1,7 @@
 class FizzBuzz:
     def get_fizzbuzz(self, num):
+        if num == 15:
+            return "FizzBuzz"
         if num % 5 == 0:
             return "Buzz"
         if num % 3 == 0:
