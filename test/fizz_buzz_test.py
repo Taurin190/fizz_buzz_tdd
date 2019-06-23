341038412fec2568ec97f6be3244c9e51c8d913f
Koichi Taura <taura.koichi@gmail.com>
2019-06-23 21:36:02 +0900

    add red pattern test for 3 times number requirement

--- test/fizz_buzz_test.py
+++ test/fizz_buzz_test.py
@@ -6,6 +6,11 @@ class FizzBuzzTest(TestCase):
     def test_1を入力した時に文字列で返す(self):
         # 準備
         fizzbuzz = FizzBuzz()
-        #
         # 実行 & 検証
         self.assertEqual("1", fizzbuzz.get_fizzbuzz(1))
+
+    def test_3を入力した時にFizzで返す(self):
+        # 準備
+        fizzbuzz = FizzBuzz()
+        # 実行 & 検証
+        self.assertEqual("Fizz", fizzbuzz.get_fizzbuzz(3))
