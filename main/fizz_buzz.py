2192b7698a70fbe0f94a5eda65c52fb37bb80a69
Koichi Taura <taura.koichi@gmail.com>
2019-06-24 23:32:23 +0900

    add green pattern and refactored

--- main/fizz_buzz.py
+++ main/fizz_buzz.py
@@ -1,5 +1,9 @@
 class FizzBuzz:
     def get_fizzbuzz(self, num):
+        if type(num) != int:
+            return ""
+        elif 0 >= num or num > 100:
+            return ""
         if num % 15 == 0:
             return "FizzBuzz"
         elif num % 5 == 0:
